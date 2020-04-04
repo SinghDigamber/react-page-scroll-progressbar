ef1dfdbc500dd5a7f1210f826b7bd709a67b47ce
Digamber Singh <digamber@positronx.io>
2020-04-04 06:00:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scroll: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.progressBar);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.progressBar);
+  }
+
+  progressBar = () => {
+    const scrollTotal = document.documentElement.scrollTop;
+    const heightWin = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scroll = `${scrollTotal / heightWin * 100}%`;
+
+    this.setState({
+      scroll: scroll
+    });
+  };
+
+  render() {
+    const progressMainWrapper = {
+      background: "rgba(255, 255, 255, 0.14)",
+      height: "15px",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      zIndex: 101,
+      width: "100%"
+    };
+
+    const progressMainStyle = {
+      height: "15px",
+      background: "#00cc83",
+      width: this.state.scroll
+    };
+
+    return (
+        <div className="progress-bar" style={progressMainWrapper}>
+          <div style={progressMainStyle} />
+        </div>
+    );
+  }
 }
 
 export default App;
+
